80692e4d5373bbe1f5909e80dae0fd801944cd19
dhnchandan <php.chandan@gmail.com>
2021-01-31 22:34:11 +0600

    Test: Implemented enzyme for testing

--- src/App/Tests/App.test.tsx
+++ src/App/Tests/App.test.tsx
@@ -1,9 +1,29 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
 
-test("renders learn react link", () => {
-	render(<App />);
-	const linkElement = screen.getByText(/React Starter/i);
-	expect(linkElement).toBeInTheDocument();
+import { shallow, ShallowWrapper } from "enzyme";
+
+import App from "../App";
+import ExampleComponent from "../../store/ExampleComponent";
+
+describe("App", () => {
+	let wrapper: ShallowWrapper;
+	beforeAll(() => {
+		wrapper = shallow(<App />);
+	});
+
+	it("should render correctly", () => {});
+	it("should contain word `React`", () => {
+		const header = wrapper.find("h1");
+		expect(header.text()).toMatch(/React/);
+	});
+	it("should contain one header", () => {
+		const header = wrapper.find("h1");
+		expect(header.length).toEqual(1);
+	});
+	it("should render the ExampleComponent Component", () => {
+		expect(wrapper.containsMatchingElement(<ExampleComponent />)).toEqual(
+			true
+		);
+	});
+	it("should render correctly", () => expect(wrapper).toMatchSnapshot());
 });
