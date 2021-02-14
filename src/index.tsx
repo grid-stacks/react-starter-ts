50b75a2a99cb1b8afdb0957a95625f718db12a25
DHN Chandan <php.chandan@gmail.com>
2021-02-14 13:40:13 +0600

    feat: :lipstick: Add Material UI
    Material UI implemeted for UI design.

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,7 @@ import { ReactReduxFirebaseProvider } from "react-redux-firebase";
 import { rrfProps } from "./store/_providers/firebase";
 
 import App from "./App/App";
+import "./styles/index.css";
 
 ReactDOM.render(
 	<StrictMode>
